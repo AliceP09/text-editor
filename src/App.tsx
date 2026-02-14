fc560c9bf67b90f6380b80bebaec25f15187316e
alice.09pereira7@gmail.com <alice.09pereira7@gmail.com>
2026-02-14 09:46:33 -0300

    primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { QueryClientProvider, QueryClient } from 'react-query';
+import { ToastContainer } from 'react-toastify';
+import { EditorContextProvider } from './context/editor-context';
+
+import 'react-toastify/dist/ReactToastify.css';
+
+import { Router } from './router';
+
+function App() {
+	const queryClient = new QueryClient()
+
+    return (
+		<QueryClientProvider client={queryClient}>
+			<EditorContextProvider>
+				<Router />
+				<ToastContainer
+					position="top-right"
+					autoClose={false}
+					closeOnClick
+				/>
+			</EditorContextProvider>
+		</QueryClientProvider>
+    );
+}
+
+export default App;
